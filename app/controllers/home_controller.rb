db82e9ad566a08b9c4cd7be7abd40c3acde9a485
kamylaaragao <kamyla.aragao55@gmail.com>
2019-01-07 10:15:53 -0200

    Administrador filtra transações por moeda

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,18 @@
 class HomeController < ApplicationController
   def index
-    @transactions = Transaction.all
+    if params[:currency]
+      @transactions = Transaction.where(nil)
+      filtering_params(params).each do |key, value|
+        @transactions = @transactions.public_send(key, value) if value.present?
+      end
+    else
+      @transactions = Transaction.all
+    end
+  end
+
+  private
+
+  def filtering_params(params)
+    params.slice(:currency)
   end
 end
